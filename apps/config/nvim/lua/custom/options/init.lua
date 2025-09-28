e0a46ba8000e7712f269b6cb8f9b89218061c9e0
mmynk <mohit.ritanil@gmail.com>
2025-09-28 16:25:33 -0700

    re-add nvim config as a dir

---
+++ apps/config/nvim/lua/custom/options/init.lua
@@ -0,0 +1,8 @@
+return {
+  setup = function()
+    -- Show relative line numbers
+    vim.opt.relativenumber = true
+    -- Keep the absolute line number for current line
+    vim.opt.number = true
+  end,
+}
