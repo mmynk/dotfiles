15b6fd19e48f9162f99ab2104da57991e0ede4b5
cueo <mohit.ritanil@gmail.com>
2025-09-28 16:48:07 -0700

    fix: nix installation on macOS

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -1,88 +1,48 @@
 {
-  description = "BatBook Darwin system flake";
+  description = "BatBook nix-darwin system flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
-    configuration = { pkgs, config, ... }: {
+    configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages = with pkgs; [
-          android-tools
-          awscli2
-          bat
-          eza
-          fzf
-          gallery-dl
-          gh
-          go
-          graphviz
-          imagemagick
-          iterm2
-          mkalias
-          neovim
-          nodejs
-          python3
-          restish
-          ripgrep
-          starship
-          tmux
-          watch
-          yt-dlp-light
-          yazi
-          zoxide
-          zellij
-        ];
-
-      fonts.packages = [
-        (pkgs.nerdfonts.override {fonts = ["JetBrainsMono"]; })
+        bat
+        eza
+        fzf
+        neovim
+        python3
+        ripgrep
+        starship
+        tmux
+        zoxide
       ];
 
-      system.activationScripts.applications.text = let
-        env = pkgs.buildEnv {
-          name = "system-applications";
-          paths = config.environment.systemPackages;
-          pathsToLink = "/Applications";
-        };
-      in
-        pkgs.lib.mkForce ''
-        # Set up applications.
-        echo "setting up /Applications..." >&2
-        rm -rf /Applications/Nix\ Apps
-        mkdir -p /Applications/Nix\ Apps
-        find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-        while read src; do
-          app_name=$(basename "$src")
-          echo "copying $src" >&2
-          ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-        done
-      '';
-
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
+      fonts.packages = [
+        pkgs.nerd-fonts.jetbrains-mono
+      ];
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
+      # Enable alternative shell support in nix-darwin.
       # programs.fish.enable = true;
+      programs.zsh.enable = true;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
 
       # Used for backwards compatibility, please read the changelog before changing.
       # $ darwin-rebuild changelog
-      system.stateVersion = 5;
+      system.stateVersion = 6;
 
       # The platform the configuration will be used on.
-      # Apple Silicon chip is aarch64.
       nixpkgs.hostPlatform = "aarch64-darwin";
     };
   in
@@ -90,12 +50,7 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#<username>
     darwinConfigurations."<username>" = nix-darwin.lib.darwinSystem {
-      modules = [
-        configuration
-      ];
+      modules = [ configuration ];
     };
-
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."<username>".pkgs;
   };
 }
