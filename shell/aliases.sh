6fb64cbab3a3585ee3b43d12f34a8bfdf0a11b18
cueo <mohit.ritanil@gmail.com>
2025-10-26 19:31:30 -0700

    alias rm to trash

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -3,6 +3,11 @@ alias vi=nvim
 alias ls=eza
 alias l='ls -l --icons=auto'
 
+# alias rm to trash if trash cli exists
+if command -v trash &> /dev/null; then
+  alias rm='trash'
+fi
+
 nix_update() {
   cd ~/.config/nix
   nix flake update && echo "nix flake update done..."
