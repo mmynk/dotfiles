00240755f71565683113c364ee33165a0c2c923e
mmynk <mohit.ritanil@gmail.com>
2026-03-24 22:39:58 +0000

    linux/home.nix: migrate to fish, update packages, use placeholder symlinks

--- linux/home.nix
+++ linux/home.nix
@@ -6,42 +6,29 @@
   home.stateVersion = "23.05";
   home.packages = with pkgs; [
     bat
+    eza
+    fd
+    fish
     fzf
-    gh
-    go
-    oh-my-zsh
-    python3
+    less
+    mosh
+    neovim
+    ripgrep
+    starship
+    trash-cli
     zoxide
-    zsh
   ];
 
-  home.file.".zshrc" = {
-    source = config.lib.file.mkOutOfStoreSymlink /home/mmayank/dev/dotfiles/shell/zshrc;
+  home.file.".config/fish/config.fish" = {
+    source = config.lib.file.mkOutOfStoreSymlink <pwd>/shell/config.fish;
   };
 
-  home.file.".aliases" = {
-    source = config.lib.file.mkOutOfStoreSymlink /home/mmayank/dev/dotfiles/shell/aliases.sh;
+  home.file.".aliases.fish" = {
+    source = config.lib.file.mkOutOfStoreSymlink <pwd>/shell/aliases.fish;
   };
 
-  home.file.".oh-my-zsh/themes/custom-rr.zsh-theme" = {
-    source = config.lib.file.mkOutOfStoreSymlink /home/mmayank/dev/dotfiles/shell/custom-rr.zsh-theme;
-  };
-
-  home.file.".vimrc" = {
-    source = config.lib.file.mkOutOfStoreSymlink /home/mmayank/dev/dotfiles/apps/vimrc;
-  };
-
-  programs.git = {
-    enable = true;
-    userName = "mmynk";
-    userEmail = "mohit.ritanil@gmail.com";
-  };
-
-  programs.neovim = {
-    enable = true;
-    plugins = with pkgs.vimPlugins; [
-      LazyVim
-    ];
+  home.file.".config/starship.toml" = {
+    source = config.lib.file.mkOutOfStoreSymlink <pwd>/config/starship.toml;
   };
 
   programs.home-manager.enable = true;
