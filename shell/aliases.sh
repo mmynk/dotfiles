bbc5f4fe0f5515bb4adcce1cdfdfcbd5b1fdc215
mmynk <mohit.ritanil@gmail.com>
2026-03-24 21:15:57 -0700

    update aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,18 +1,19 @@
 alias cat="bat -p --theme=ansi"
 alias vi=nvim
-alias ls=eza
-alias l='ls -l --icons=auto'
-
-alias rm='rm -i'
+alias s='kitten ssh'
+alias ls='eza --group-directories-first --icons --hyperlink'
+alias ll='ls --long --header'
+alias l=ls
+alias rm='trash'
 
 nix_update() {
   cd ~/.config/nix
   nix flake update && echo "nix flake update done..."
-  nix store gc && echo "nix store gc done..."
-  cd -
 }
 
-nix_rebuild() {
+alias nix_gc='nix store gc && echo "nix store gc done..."'
+
+nix_install() {
   # For Linux
   # nix run nixpkgs#home-manager -- switch --flake ~/.config/nix
   # echo "nix home-manager rebuild done..."
@@ -21,3 +22,10 @@ nix_rebuild() {
   sudo darwin-rebuild switch --flake ~/.config/nix#${USER}
   echo "nix darwin-rebuild done..."
 }
+
+nix_rebuild() {
+  nix_update && nix_gc
+  nix_install
+}
+
+[[ -f ~/.extra_aliases ]] && source ~/.extra_aliases
