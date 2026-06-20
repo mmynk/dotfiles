dc66d2ea698c1b37cce5310c091f834b4d884411
mmynk <mohit.ritanil@gmail.com>
2026-06-20 23:28:15 +0000

    enable true color over SSH; add machine-specific aliases file
    - tmux: broaden RGB terminal-override to all terminals (*:RGB)
    - fish: upgrade bare xterm to xterm-256color over SSH so tmux/catppuccin get true color
    - add placeholder shell/extra_aliases.fish for machine-specific aliases

--- shell/config.fish
+++ shell/config.fish
@@ -2,6 +2,11 @@
 set -gx VISUAL nvim
 set -gx EDITOR nvim
 
+# Upgrade bare xterm to 256color over SSH so tmux/catppuccin get true color
+if test "$TERM" = "xterm"; and set -q SSH_CONNECTION
+    set -gx TERM xterm-256color
+end
+
 # Integrations
 starship init fish | source
 fzf --fish | source
